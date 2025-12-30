4b596d9c92050b4cefeb672bb7f27d4d79b0f320
mathisMAGNIN <mathis.magnin1@etu.univ-cotedazur.fr>
2025-12-31 00:38:37 +0100

    feat: Ajout de la definition et de l'utilisation de composants. Il reste la validation sémantique ainsi que la modification d'attributs des box et composants.

--- slideDeckML/src/cli/src/generator.ts
+++ slideDeckML/src/cli/src/generator.ts
@@ -1,15 +1,33 @@
-import type { Box, Model, Slide, TextBox } from 'slide-deck-ml-language';
+import { type ComponentBoxReference, type Box, type Model, type Slide, type TextBox, ComponentBox, ComponentSlot, ComponentSlotReference } from 'slide-deck-ml-language';
 
 import * as fs from 'node:fs';
 import * as path from 'node:path';
-import { expandToNode, toString } from 'langium/generate';
+import { CompositeGeneratorNode, expandToNode, toString } from 'langium/generate';
 import { extractDestinationAndName } from './util.js';
+
+
+/* Generate file */
 
 export function generateOutput(model: Model, filePath: string, destination: string): string {
     const data = extractDestinationAndName(destination);
     const generatedFilePath = `${path.join(data.destination, data.name)}.html`;
 
-    const fileNode = expandToNode`
+    const fileNode = generateModel(model).appendNewLineIfNotEmpty();
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(generatedFilePath, toString(fileNode));
+
+    return generatedFilePath;
+}
+
+
+/* Nodes generation */
+
+
+function generateModel(model: Model): CompositeGeneratorNode {
+    return expandToNode`
         <!doctype html>
         <html>
         <head>
@@ -27,12 +45,12 @@ export function generateOutput(model: Model, filePath: string, destination: stri
             </style>
         </head>
         <body>
-            <h1>Debug: ${model.name}</h1>
+            <h1>${model.name}</h1>
             <hr/>
             
             <div class="reveal">
                 <div class="slides">
-                    ${model.slides.map(slide => generateSlide(slide))}
+                    ${model.slides.map((slide: Slide) =>  generateSlide(slide)).join('')}
                 </div>
             </div>
             
@@ -51,49 +69,59 @@ export function generateOutput(model: Model, filePath: string, destination: stri
             </script>
         </body>
         </html>
-    `.appendNewLineIfNotEmpty();
-
-    if (!fs.existsSync(data.destination)) {
-        fs.mkdirSync(data.destination, { recursive: true });
-    }
-    fs.writeFileSync(generatedFilePath, toString(fileNode));
-
-    return generatedFilePath;
-}
-
-function generateTextBox(textBox: TextBox): string {
-    return `<p>${textBox.text}</p>`;
-}
-
-function generateBox(box: Box): string {
-    const content = box.content;
-
-    if (content.$type === 'TextBox') {
-        return generateTextBox(content);
-    }
-
-    if (content.$type === 'ContentBox') {
-        const boxes = content.boxes.map(b => generateBox(b)).join('\n');
-        return `
-            <div>
-                ${boxes}
-            </div>
-        `;
-    }
-
-    return '';
+    `;
 }
 
 function generateSlide(slide: Slide): string {
-    const header = slide.header ? generateBox(slide.header.box) : '';
-    const body = generateBox(slide.body.box);
-    const footer = slide.footer ? generateBox(slide.footer.box) : '';
-
     return `
         <section id="${slide.id}" data-transition="fade">
-            ${header}
-            ${body}
-            ${footer}
+            ${generateBox(slide.content)}
         </section>
     `;
+}
+
+function generateBox(box: Box): string {
+    switch(box.content.$type) {
+        case 'TextBox': return generateTextBox(box.content);
+        case 'ComponentBoxReference': return generateComponentBoxReference(box.content);
+
+        case 'ContentBox':
+            return `
+                <div>
+                    ${box.content.boxes.map(b => generateBox(b)).join('\n')}
+                </div>
+            `;
+    }
+}
+
+function generateTextBox(textBox: TextBox): string {
+    return `<p>${textBox.content.slice(1, -1).trim()}</p>`;
+}
+
+function generateComponentBoxReference(reference: ComponentBoxReference): string {
+    const declaration = reference.reference.ref!; // TODO: check if the ref is valid
+    let slots: Record<string, string> = {};
+    for (let slot of reference.slots) {
+        slots[slot.name] = generateBox(slot.content);
+    }
+    return generateComponentBox(declaration.content, slots);
+}
+
+function generateComponentBox(box: ComponentBox, slots: Record<string, string>): string {
+    switch(box.content.$type) {
+        case 'TextBox': return generateTextBox(box.content);
+        case 'ComponentBoxReference': return generateComponentBoxReference(box.content);
+
+        case 'ComponentSlot': return generateComponentSlot(box.content, slots);
+        case 'ComponentContentBox':
+            return `
+                <div>
+                    ${box.content.boxes.map(box => generateComponentBox(box, slots)).join('\n')}
+                </div>
+            `;
+    }
+}
+
+function generateComponentSlot(slot: ComponentSlot, slots: Record<string, string>): string {
+    return slots[slot.name] || '';
 }
